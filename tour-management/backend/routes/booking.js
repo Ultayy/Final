f0ee409a889b9aa4ec43b65f7189737e474be997
Ultayy <131360012+Ultayy@users.noreply.github.com>
2023-12-20 23:38:41 +0600

    Delete tour-management/backend directory

--- tour-management/backend/routes/booking.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {verifyAdmin, verifyUser} from "../utils/Token.js";
-import {createTourBooking, getAllBooking, getBooking} from "../controllers/tourBookingController.js";
-
-const router = express.Router()
-router.post('/',verifyUser, createTourBooking)
-router.post('/:id',verifyUser, getBooking)
-router.post('/',verifyAdmin, getAllBooking)
-export default router
